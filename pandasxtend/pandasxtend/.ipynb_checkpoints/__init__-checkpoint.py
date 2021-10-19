4a3f791b1598c4a136606ef419670758e21c530f
LAPTOP-FNOEDJV3\k2J <debreulang@gmail.com>
2021-10-20 00:39:49 +0900

    delete checkpoints

--- pandasxtend/pandasxtend/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,2 +0,0 @@
-#from .catdap import catdap
-#from .eda import eda
--- pandasxtend/pandasxtend/eda/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,8 +0,0 @@
-from ..eda import stats
-from ..eda.stats import KL_Divergence
-from ..eda.stats import Summary
-from ..eda.visualization import AllPlot
-
-
-
-
